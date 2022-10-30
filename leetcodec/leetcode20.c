66b1a3d2eae87ac4122fbd354aa5d693d31c72ac
pz <peteryzhang518@gmail.com>
2022-10-30 23:20:18 +0000

    add easy question

--- leetcodec/leetcode20.c
+++ leetcodec/leetcode20.c
@@ -1,5 +1,25 @@
+/*给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+每个右括号都有一个对应的相同类型的左括号。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/valid-parentheses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
 #include <stdio.h>
 #include <stdlib.h>
+#incldue <string.h>
+
+bool isValid(char * s){
+    int num = strlen(s)/2;
+    char [num]={0};
+    for(int i=0; i<num; i++){
+        char
+    }
+}
 
 int main(){
 
---
+++ leetcodec/leetcode83.c
@@ -0,0 +1,32 @@
+/*给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+
+ 
+
+示例 1：
+
+
+输入：head = [1,1,2]
+输出：[1,2]
+示例 2：
+
+
+输入：head = [1,1,2,3,3]
+输出：[1,2,3]
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/remove-duplicates-from-sorted-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* deleteDuplicates(struct ListNode* head){
+
+}
