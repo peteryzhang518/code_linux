150fc1d9e5d1c7dfc5982ea2d92f2814f8b04f04
pz <peteryzhang518@gmail.com>
2022-10-31 22:43:48 +0000

    successed code

--- leetcodec/leetcode20.c
+++ leetcodec/leetcode20.c
@@ -9,21 +9,63 @@
 来源：力扣（LeetCode）
 链接：https://leetcode.cn/problems/valid-parentheses
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#incldue <string.h>
+#include <string.h>
 
 bool isValid(char * s){
     int num = strlen(s)/2;
-    char [num]={0};
-    for(int i=0; i<num; i++){
-        char
+    int pointer =1;
+    char* stack = (char *)malloc(2*num+2);
+    for(int i=0; i<strlen(s); i++){
+        if(*(s+i)=='['||*(s+i)=='{'||*(s+i)=='('){
+            *(stack+pointer)=*(s+i);
+            pointer++;
+        }
+        else if(*(s+i)==']'){
+            if(*(stack+pointer-1)=='['){
+                pointer--;
+                *(stack+pointer)='0';
+            } 
+            else{
+                *(stack+pointer)=*(s+i);
+                pointer++;
+            }
+        }
+        else if(*(s+i)=='}'){
+            if(*(stack+pointer-1)=='{'){
+                pointer--;
+                *(stack+pointer)='0';
+            } 
+            else{
+                *(stack+pointer)=*(s+i);
+                pointer++;
+            }
+        }
+        else if(*(s+i)==')'){
+            if(*(stack+pointer-1)=='('){
+                pointer--;
+                *(stack+pointer)='0';
+            } 
+            else{
+                *(stack+pointer)=*(s+i);
+                pointer++;
+            }
+        }
+    }
+    if(pointer==1&&strlen(s)!=1){
+        return true;
+    }
+    else{
+        return false;
     }
 }
 
 int main(){
-
-
+    char* temp = "[()]{";
+    int result = isValid(temp);
+    printf("result = %d",result);
 
     return 0;
 }
